2065961e924333480b095e85c20606bfdfc7c2eb
digvijay sinh rathod <155974214+digvijaysinhx1@users.noreply.github.com>
2026-02-04 23:54:33 +0530

    Performance & Accesibilites Update 2

--- src/components/Section 1/Sec1Hero.jsx
+++ src/components/Section 1/Sec1Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Header/Header'
-import Bg from '../../assets/AllBg.webp'
+import Bg1600 from '../../assets/AllBg.webp'
+import Bg900 from '../../assets/AllBg-mobile.webp'
 import AnalyticCard from '../Analytics Card/AnalyticCard'
 import TeamList from '../Team List/TeamList'
 import Employee from '../Employee/Employee'
@@ -8,15 +9,18 @@ import HeroCenter from '../Hero Center Text/HeroCenter'
 const Sec1Hero = () => {
   
   return (
-    <div className=' flex flex-col relative h-fit justify-start items-center w-full md:h-screen bg-no-repeat bg-cover bg-center' >
+    <div className=' flex flex-col relative h-screen justify-start items-center w-full md:h-screen bg-no-repeat bg-cover bg-center' >
       <img
-src={Bg}
-alt="bg vector"
-className="absolute inset-0 w-full h-full object-cover"
-loading='lazy'
-decoding="async"
-fetchPriority='high'
+  src={Bg900}
+  srcSet={`${Bg900} 900w, ${Bg1600} 1600w`}
+  sizes="(max-width: 768px) 100vw, 1600px"
+  alt=""
+  className="absolute inset-0 w-full h-full object-cover"
+  loading="eager"
+  decoding="async"
+  fetchPriority="high"
 />
+
       <Header/>
       <div>
       <HeroCenter/>
